deee090c2004a3a67adffceb0b8e73053764e13f
ManhCuongOke <cuong03654855879@gmail.com>
2024-08-23 22:53:19 +0700

    project

---
+++ Controllers/LayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Controllers
+{
+    public class LayoutController : Controller
+    {
+        public IActionResult Layout()
+        {
+            return View();
+        }
+    }
+}
